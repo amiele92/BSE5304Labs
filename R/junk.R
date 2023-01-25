cbc4e12a4701617ca8a300bbefddcd94a6614b1d
Your Name <you@example.com>
2023-01-25 20:40:14 +0000

    test

--- R/junk.R
+++ R/junk.R
@@ -1,3 +1,5 @@
-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
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+#hello
+#ghp_Fmy2gctaYHfcIkEf
+#7PEpQBqosxsDJ61Q8aek
+system('git config --global user.email "you@example.com"')
+system('git config --global user.name "Your Name"')
